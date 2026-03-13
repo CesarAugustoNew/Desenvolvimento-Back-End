3534579f9f32ea10c32199bd5e4f20a6a1835f3a
FIC <fic@info.local>
2026-03-13 19:14:20 -0300

    primeiro commit

---
+++ aula01/src/Aula01.java
@@ -0,0 +1,33 @@
+public class Aula01 {
+    public static void main(String[] args) {
+        //Saida de dados.
+        System.out.println(5 + 5);
+        System.out.println("Cesar Augusto");
+        System.out.println("Cesar" + 5); // Concatenação
+
+        int idade = 22;
+        System.out.printf("Cesar Augusto %d", idade); // interpolação
+
+        /*        Tipos de dados
+
+             int = numeros inteiros
+             double/float = decimais
+             string = texto
+             boolean = true/false
+             %s para string
+             %d para inteiros
+             %f para float/double */
+
+        // entrada de dados
+        int num1 = 50;
+        double num2 = 20.3;
+
+        //processamento de dados
+        double resultado = num1 + num2;
+
+        System.out.printf("\nA soma de num1 + num2 = %f", resultado);
+
+
+        
+    }
+}
