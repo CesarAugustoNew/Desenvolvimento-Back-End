b9a13129a5e32996bb307b1e140b8586758048ae
cesar augusto <cesarprofissional22@gmail.com>
2026-03-17 21:59:51 -0300

    condicionais

--- aula01/src/aulaQuatro.java
+++ aula01/src/aulaQuatro.java
@@ -6,7 +6,8 @@ public class aulaQuatro {
         boolean documento = false;
 
         if (idade >= 18 && documento == true) {
-            System.out.println("Pode entrar na balada");
+            System.out.println("Pode entrar na balada" +
+                    "");
         }else {
             System.out.println("Não pode entra na balada");
         }
