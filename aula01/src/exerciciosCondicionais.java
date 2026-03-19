8f0aa96cd721ce254dd81b5e3a7576ed897cb198
cesar augusto <cesarprofissional22@gmail.com>
2026-03-19 19:32:43 -0300

    corrigindo exercicios

--- aula01/src/exerciciosCondicionais.java
+++ aula01/src/exerciciosCondicionais.java
@@ -6,9 +6,9 @@ public class exerciciosCondicionais {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Exercício 2 — Número Par ou Impar
-
-//        System.out.println("Digite um numero");
+//      //  Exercício 1 — Número Par ou Impar
+//
+//        System.out.println("Digite um numero para ver se é par ou impar: ");
 //        int numero = scanner.nextInt();
 //
 //        if (numero % 2 == 0) {
@@ -16,22 +16,30 @@ public class exerciciosCondicionais {
 //        }else {
 //            System.out.println("Numero é impar");
 //        }
-
-     //   Exercício 2 — Número Positivo ou Negativo
-
-//        System.out.println("Digite um numero");
-//        int numero = scanner.nextInt();
 //
-//        if (numero > 0) {
+//
+//
+//
+//
+//      //  Exercício 2 — Número Positivo ou Negativo
+//
+//        System.out.println("Digite um numero");
+//        int numeroPositivoNegativo = scanner.nextInt();
+//
+//        if (numeroPositivoNegativo > 0) {
 //            System.out.println("Numero positivo");
-//        } else if (numero < 0) {
+//        } else if (numeroPositivoNegativo < 0) {
 //            System.out.println("Numero negativo");
 //        }else {
 //            System.out.println("Numero é zero");
 //        }
-
-        // Exercício 3 — Maior de Idade
-
+//
+//
+//
+//
+//
+//      //   Exercício 3 — Maior de Idade
+//
 //        System.out.println("Digite a sua idade");
 //        int idadeUsuario = scanner.nextInt();
 //
@@ -40,9 +48,12 @@ public class exerciciosCondicionais {
 //        } else {
 //            System.out.println("Voce e menor de idade");
 //        }
-
-        // Exercício 4 — Classificação de Nota
-
+//
+//
+//
+//
+//      //   Exercício 4 — Classificação de Nota
+//
 //        System.out.println("Digite a sua nota: ");
 //        int notaAluno = scanner.nextInt();
 //
@@ -53,9 +64,13 @@ public class exerciciosCondicionais {
 //        }else {
 //            System.out.println("Voce esta reprovado");
 //        }
-
-        // Exercício 5 — Maior entre dois números
-
+//
+//
+//
+//
+//
+//      //   Exercício 5 — Maior entre dois números
+//
 //        System.out.println("Digite o primeiro numero: ");
 //        int numeroUm = scanner.nextInt();
 //
@@ -63,15 +78,18 @@ public class exerciciosCondicionais {
 //        int numeroDois = scanner.nextInt();
 //
 //        if (numeroUm > numeroDois) {
-//            System.out.printf("Numero %s > %s", numeroUm, numeroDois);
+//            System.out.printf("Numero um maior que numero dois %s > %s", numeroUm, numeroDois);
 //        } else if (numeroDois > numeroUm) {
-//            System.out.printf("Numero %s > %s", numeroDois, numeroUm);
+//            System.out.printf("Numero dois maior que numero um %s > %s", numeroDois, numeroUm);
 //        } else {
 //            System.out.println("Os numeros são iguais");
 //        }
-
-        // Exercício 6 — Menu com Switch
-
+//
+//
+//
+//
+//      //   Exercício 6 — Menu com Switch
+//
 //        System.out.println("1. Cadastrar ");
 //        System.out.println("2. Editar");
 //        System.out.println("3. Excluir");
@@ -96,9 +114,13 @@ public class exerciciosCondicionais {
 //            default:
 //                System.out.println("Opção inválida!");
 //        }
-
-//        Exercício 7 — Dia da Semana
-
+//
+//
+//
+//
+//
+//      //  Exercício 7 — Dia da Semana
+//
 //        System.out.println("Digite um dia da semana de 1 a 7: ");
 //        int diaDaSemana = scanner.nextInt();
 //
@@ -128,16 +150,18 @@ public class exerciciosCondicionais {
 //                System.out.println("Opção invalida");
 //                break;
 //        }
-
-
-
-//              Exercício 8 — Calculadora Simples
-
+//
+//
+//
+//
+//
+//       //       Exercício 8 — Calculadora Simples
+//
 //        System.out.println("Digite o primeiro numero: ");
-//        int numeroUm = scanner.nextInt();
+//        int numeroCalUm = scanner.nextInt();
 //
 //        System.out.println("Digite o segundo numero: ");
-//        int numeroDois = scanner.nextInt();
+//        int numeroCalDois = scanner.nextInt();
 //
 //        System.out.println("Escola um operador");
 //        System.out.println("1. +");
@@ -149,28 +173,32 @@ public class exerciciosCondicionais {
 //
 //        switch (operador) {
 //            case 1:
-//                int soma = numeroUm + numeroDois;
+//                int soma = numeroCalUm + numeroDois;
 //                System.out.printf("A soma dos numeros é %s", soma);
 //                break;
 //            case 2 :
-//                int subtracao = numeroUm - numeroDois;
+//                int subtracao = numeroCalUm - numeroCalDois;
 //                System.out.printf("A subtracao dos numeros é %s", subtracao);
 //                break;
 //            case 3 :
-//                int multiplicacao = numeroUm * numeroDois;
+//                int multiplicacao = numeroCalUm * numeroCalDois;
 //                System.out.printf("A multiplicação dos numeros é %s", multiplicacao);
 //                break;
 //            case 4 :
-//                double divisao = (double) numeroUm / numeroDois;
-//                System.out.printf("A divisão dos numeros é %.2f", diviso);
+//                double divisao = (double) numeroCalUm / numeroCalDois;
+//                System.out.printf("A divisão dos numeros é %.2f", divisao);
 //                break;
 //            default:
 //                System.out.println("Opcao invalida");
 //                break;
 //        }
-
-        // Exercício 9 — Classificação de Temperatura
-
+//
+//
+//
+//
+//
+//     //   Exercício 9 — Classificação de Temperatura
+//
 //        System.out.println("Digite uma tempetarura");
 //        int tempetaruta = scanner.nextInt();
 //
@@ -184,23 +212,31 @@ public class exerciciosCondicionais {
 //            System.out.println("Muito frio");
 //        }
 
+//
+//
+//        //      Exercício 10 — Login Simples
+//
 //        System.out.println("Digite seu usuario: ");
 //        String usuario = scanner.nextLine();
 //
 //        System.out.println("Digite a sua senha: ");
-//        int senha = scanner.nextInt();
+//        String senha = scanner.nextLine();
 //
 //        String usuarioCorreto = "admin";
-//        int senhaCorreta = 1234;
+//        String senhaCorreta = "1234";
 //
-//        if (!usuario.equals(usuarioCorreto) || senha != senhaCorreta) {
+//        if (!usuario.equals(usuarioCorreto) || senha.equals(senhaCorreta)) {
 //            System.out.println("Usuario ou senha incorreto.");
 //        }else{
 //            System.out.println("Bem vindo ao sistema.");
 //        }
-
-        // Desafio 1 — Múltiplas Condições (Pode Dirigir)
-
+//
+//
+//
+//
+//
+//       //  Desafio 1 — Múltiplas Condições (Pode Dirigir)
+//
 //        System.out.println("Digite a sua idade");
 //        int idade = scanner.nextInt();
 //
