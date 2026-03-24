8f0e3aadf6d601a16b3b4b60557307c138880173
cesar augusto <cesarprofissional22@gmail.com>
2026-03-24 19:51:37 -0300

    iniciando Arrays

---
+++ Arrays-list/Arrays/src/Arrays01.java
@@ -0,0 +1,10 @@
+public class Arrays01 {
+    public static void main(String[] args) {
+        //array - posições fixas (definir)
+        //array -> tipos unicos (homogenios)
+        //arrays -> sempre começarão com indice 0
+        // arrays -> tipo[] nomeGenerico = new tipo[tamanho];
+
+        String[] cestaFrutas = new String[5];
+    }
+}
